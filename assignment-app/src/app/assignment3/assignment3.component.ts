f36373ffbc3d2a57dc39990e5f2a6549867f27ae
afrin.zakir <Thaleb@98>
2022-08-17 16:51:59 +0530

    add index in ngFor

--- assignment-app/src/app/assignment3/assignment3.component.ts
+++ assignment-app/src/app/assignment3/assignment3.component.ts
@@ -8,11 +8,12 @@ import { Component } from "@angular/core";
 export class Assignment3 {
 
     showSecret: boolean = false;
-    log: number[] = [];
+    log: any = [];
 
     onToggleDetails() {
         this.showSecret = !this.showSecret;
-        this.log.push(this.log.length + 1);
+        //this.log.push(this.log.length + 1);
+        this.log.push(new Date());
     }
 
 }
